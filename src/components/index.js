754940ed8ce5db087722f907fbb03e5b1e50d73a
kolomoitsev <Pavel27011999138>
2020-06-16 15:57:32 +0300

    small fix

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,4 @@
 export {default as Header} from './header/header.component'
 export {default as Auth} from './auth/auth.component'
 export {default as Register} from './register/register.component'
-export {default as Translater} from './translator/translater.component'
+export {default as Translater} from './translater/translater.component'
